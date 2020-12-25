537babba05408478540adb045157fdb52e34332c
Nguyen Thanh Huy <huy.nguyenthanh@caters.co.jp>
2020-12-25 18:49:26 +0900

    second commit

---
+++ server.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const Hapi = require("hapi");
+const Settings = require("./settings");
+const Routes = require("./lib/routes");
+const Models = require("./lib/models/");
+
+
+const init = async () => {
+    const server = new Hapi.Server({ port: Settings.port });
+
+    server.route(Routes);
+    await Models.sequelize.sync();
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", err => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
