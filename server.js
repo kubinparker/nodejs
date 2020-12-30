b308e650dc959fe99c75b6563a891aca0f7b32c8
Nguyễn Thanh Huy <ba.team.vnp@gmail.com>
2020-12-30 15:13:51 +0900

    add form and test CRUD

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 "use strict";
 
-const Hapi = require("hapi");
+const Path = require("path");
+const Hapi = require("@hapi/hapi");
+
 const Settings = require("./settings");
 const Routes = require("./lib/routes");
 const Models = require("./lib/models/");
 
 
 const init = async () => {
+    /**
+     * Create server and set port for server
+     */
     const server = new Hapi.Server({ port: Settings.port });
-
+    /**
+     * hapi/vision: enable the view functionality in our server
+     * hapi/inert: define folder as public
+     */
+    await server.register([require("@hapi/vision"), require("@hapi/inert")]);
+    server.views({
+        engines: { pug: require("pug") },
+        path: Path.join(__dirname, "lib/views"),
+        compileOptions: {
+            pretty: false
+        },
+        isCached: Settings.env === "production"
+    });
+    /**
+     * Set route
+     */
     server.route(Routes);
+    /**
+     * Connect to database and mapping with table
+     */
     await Models.sequelize.sync();
+    /**
+     * Sever running
+     */
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
